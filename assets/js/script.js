0de98d1051c9a01071941f0d3dc7f2c84ecb3374
João Henrique de Souza Silva <joaohenrique7482@gmail.com>
2022-06-30 18:49:14 -0300

    refactor:

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,44 @@ const btnCreateTask = document.querySelectorAll(".btnCreateTask");
 let tasksDone = JSON.parse(localStorage.getItem("tasksDone"));
 let tasksPending = JSON.parse(localStorage.getItem("tasksPending")) 
 
-const getTasks = () => {
+function getTasks(){
   tasksDone = JSON.parse(localStorage.getItem("tasksDone"));
   tasksPending = JSON.parse(localStorage.getItem("tasksPending"))
   
   return {tasksDone, tasksPending}
 }
 
-function cleanLS() {
+function cleanLS(){
   localStorage.clear();
   location.reload();
+}
+
+function showCheck(e){
+  e.target.firstElementChild.classList.toggle('disabled')
+}
+
+function markCheck(e){
+  e.composedPath()[1].classList.toggle('checked')
+  // let ulCategoryTask = e.target.parentNode.parentNode
+  // let title = e.target.textContent
+
+  // if(ulCategoryTask.classList.contains('ulDone')){
+  //   ulPending.append(createTask(title, false))
+  //   const newPendingTasks = [...tasksPending, { title: title }];
+  //   localStorage.setItem("tasksPending", JSON.stringify(newPendingTasks));
+  //   // tasksDone.filter(e => {
+  //   //   if(e.title == title){
+  //   //     const indexTasksDone = tasksDone.indexOf(e)
+  //   //     tasksDone.splice(indexTasksDone, (indexTasksDone + 1))
+  //   //     saveOnLSBeforeDeleted(tasksDone, tasksPending)
+  //   //     // renderTask(tasksDone, tasksPending)
+  //   //     console.log(indexTasksDone)
+  //   //   }
+  //   // }) 
+  // }else{
+  //   console.log('pending')
+  // }
+  // console.log(title)
 }
 
 function createTask(titleTask, done) {
@@ -43,13 +71,8 @@ function createTask(titleTask, done) {
 }
 
 function renderTask(tasksDone, tasksPending) {
-  if (tasksDone == null) {
-    tasksDone = [];
-  }
-
-  if (tasksPending == null) {
-    tasksPending = [];
-  }
+  tasksDone == null ? tasksDone = []: tasksDone = tasksDone;
+  tasksPending == null ? tasksPending = []: tasksPending = tasksPending;
 
   ulDone.innerHTML = "";
   ulPending.innerHTML = "";
@@ -67,11 +90,11 @@ renderTask(tasksDone, tasksPending);
 function saveOnLS(e, titleTask) {
   const ulTask = e.target.parentNode.previousElementSibling.firstElementChild;
 
-  if (ulTask.classList.contains("ulDone")) {
-    const newDoneTasks = [...tasksDone, { title: titleTask, checked: true}];
+  if(ulTask.classList.contains("ulDone")) {
+    const newDoneTasks = [...tasksDone, { title: titleTask}];
     localStorage.setItem("tasksDone", JSON.stringify(newDoneTasks));
-  } else if (ulTask.classList.contains("ulPending")) {
-    const newPendingTasks = [...tasksPending, { title: titleTask, checked: false }];
+  }else{
+    const newPendingTasks = [...tasksPending, { title: titleTask}];
     localStorage.setItem("tasksPending", JSON.stringify(newPendingTasks));
   }
 }
@@ -79,6 +102,19 @@ function saveOnLS(e, titleTask) {
 function saveOnLSBeforeDeleted(tasksDone, tasksPending) {
   localStorage.setItem("tasksDone", JSON.stringify(tasksDone));
   localStorage.setItem("tasksPending", JSON.stringify(tasksPending));
+}
+
+function createElement(e) {
+  const titleTask = prompt('Digite o título da tarefa:')
+  if(titleTask == null || titleTask == ''){
+    createElement(e)
+  }else{
+    tasksDone == null ? tasksDone = []: tasksDone = tasksDone;
+    tasksPending == null ? tasksPending = []: tasksPending = tasksPending;
+    saveOnLS(e, titleTask);
+    getTasks()
+    renderTask(tasksDone, tasksPending);
+  }
 }
 
 function deleteTask(e){
@@ -105,33 +141,8 @@ function deleteTask(e){
   }
 }
 
-function createElement(e) {
-    const titleTask = prompt("Digite o título da tarefa:");
-
-    if (tasksDone == null) {
-      tasksDone = [];
-    }
-  
-    if (tasksPending == null) {
-      tasksPending = [];
-    }
-    saveOnLS(e, titleTask);
-    getTasks()
-    renderTask(tasksDone, tasksPending);
-}
-
-function showCheck(e){
-  e.target.firstElementChild.classList.toggle('disabled')
-}
-
-function markCheck(e){
-  e.composedPath()[1].classList.toggle('checked')
-  const checked = e.composedPath()[1].classList.contains('checked')
-}
-
 btnCreateTask.forEach(btn => {
   btn.addEventListener("click", (e) => {
     createElement(e)
   });
 });
-
